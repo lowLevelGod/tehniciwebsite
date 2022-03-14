7100ddcd5a43249a6fb6b47bd25f84c3c8ac751a
lowLevelGod <pete@LAPTOP-O6VB6IFH.localdomain>
2022-03-14 17:46:38 +0200

    etapa3done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express= require("express");
 app= express();
+const fs = require('fs');
+const sharp = require('sharp');
 
 app.set("view engine","ejs");
 
@@ -60,33 +62,38 @@ app.get("/*", function(req, res){
     //console.log("generala:",req.url);
     res.end();
 })
-
-
-
 /*
-
-function creeazaImagini(){
-    var buf=fs.readFileSync(__dirname+"/resurse/json/galerie.json").toString("utf8");
-    obImagini=JSON.parse(buf);//global
+function creeazaImagini() {
+    var buf = fs
+      .readFileSync(__dirname + "/resurse/json/galerie.json")
+      .toString("utf8");
+  
+    obImagini = JSON.parse(buf); //global
+  
     //console.log(obImagini);
-    for (let imag of obImagini.imagini){
-        let nume_imag, extensie;
-        [nume_imag, extensie ]=imag.fisier.split(".")// "abc.de".split(".") ---> ["abc","de"]
-        let dim_mic=150
-        
-        imag.mic=`${obImagini.cale_galerie}/mic/${nume_imag}-${dim_mic}.webp` //nume-150.webp // "a10" b=10 "a"+b `a${b}`
-        //console.log(imag.mic);
-        imag.mare=`${obImagini.cale_galerie}/${imag.fisier}`;
-        if (!fs.existsSync(imag.mic))
-            sharp(__dirname+"/"+imag.mare).resize(dim_mic).toFile(__dirname+"/"+imag.mic);
-
-        
+    for (let imag of obImagini.imagini) {
+      let nume_imag, extensie;
+      [nume_imag, extensie] = imag.fisier.split("."); // "abc.de".split(".") ---> ["abc","de"]
+      let dim_mic = 150;
+  
+      imag.mic = `${obImagini.cale_galerie}/mic/${nume_imag}-${dim_mic}.webp`; //nume-150.webp // "a10" b=10 "a"+b `a${b}`
+      //console.log(imag.mic);
+  
+      imag.mare = `${obImagini.cale_galerie}/${imag.fisier}`;
+      if (!fs.existsSync(imag.mic))
+        sharp(__dirname + "/" + imag.mare)
+          .resize(dim_mic)
+          .toFile(__dirname + "/" + imag.mic);
+  
+      let dim_mediu = 300;
+      imag.mediu = `${obImagini.cale_galerie}/mediu/${nume_imag}-${dim_mediu}.webp`;
+      if (!fs.existsSync(imag.mediu))
+        sharp(__dirname + "/" + imag.mare)
+          .resize(dim_mediu)
+          .toFile(__dirname + "/" + imag.mediu);
     }
-
-}
-creeazaImagini();
-
-*/
+  }
+  creeazaImagini();*/
 
 const hostname = "127.0.0.1";
 const port = 8080;
